4407dd6d2ac44d6efe06610a62668601751b6049
Ahmed Cherif KRID <a.cherifkrid@openbee.com>
2022-11-15 07:37:07 +0100

    first app reactjs

---
+++ first app/index.js
@@ -0,0 +1,6 @@
+ReactDom.render(<h1>Hello, everyone !</h1>, document.getElementById("root"));
+
+/*
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));*/
